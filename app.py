13302aa970e24586dc68c70a09caef027f564754
user <adruts@mail.ru>
2022-08-31 16:49:13 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>'% self.id
+
+@app.route('/')
+@app.route('/home')
+def index(): #put application's code here
+    return render_template('index.html')
+
+@app.route('/about')
+def about(): #put application's code here
+    return render_template('about.html')
+
+@app.route('/create-article')
+def create_article(): #put application's code here
+    return render_template('create-article.html')
+
+if __name__=='__main__':
+    app.run(debug=True)

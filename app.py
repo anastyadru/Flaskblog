ac9f7785027b9bbe84a01a907ecbaa9a027495c6
user <adruts@mail.ru>
2022-09-04 15:20:16 +0300

    First commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Article(db.Model):
@@ -25,9 +26,24 @@ def index(): #put application's code here
 def about(): #put application's code here
     return render_template('about.html')
 
-@app.route('/create-article')
+@app.route('/create-article', methods=['POST','GET'])
 def create_article(): #put application's code here
-    return render_template('create-article.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+        return render_template('create-article.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)

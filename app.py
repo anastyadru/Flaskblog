2f202fab148fb0efa8c19b2de082e3745e08657a
user <adruts@mail.ru>
2022-09-07 00:44:16 +0300

    First commit

--- app.py
+++ app.py
@@ -47,6 +47,25 @@ def post_delete(id):
     except:
         return 'Error'
 
+@app.route('/posts/<int:id>/update', methods=['POST','GET'])
+def post_update(id):
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error"
+    else:
+        article = Article.query.get(id)
+        return render_template('post_update.html', article=article)
+
 @app.route('/create-article', methods=['POST','GET'])
 def create_article():
     if request.method == 'POST':
